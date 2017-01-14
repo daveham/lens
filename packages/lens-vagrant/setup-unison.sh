ccaf6d4be59110acd62d7e87eafdca73da6b1988
Dave Hamilton <djhamilton2@sbcglobal.net>
2017-01-14 14:17:11 -0600

    Changes to unison profiles

--- packages/lens-vagrant/setup-unison.sh
+++ packages/lens-vagrant/setup-unison.sh
@@ -7,16 +7,32 @@ vagrant ssh-config > "$ssh_config"
 # common roots
 root_host="$PWD/.."
 root_vm="ssh://default//lens/"
+root_data_host="$PWD/../../lens-data"
+root_data_vm="ssh://default//lens/data/"
 
-# create unison profile for project1
+# create unison profile for lens projects
 profile1="
 root = $root_host
 root = $root_vm
 path = lens-data-manager
 path = lens-data-service
 ignore = Name {CHANGELOG.md,CONTRIBUTING.md,LICENSE,README.md,*.log}
-ignore = Name {.git,node_modules,DS_Store,lens-vagrant}
+ignore = Name {.git,node_modules,.DS_Store,lens-vagrant}
 prefer = $root_host
+repeat = 2
+terse = true
+dontchmod = true
+perms = 0
+sshargs = -F $ssh_config
+"
+
+# create unison profile for lens data
+profile2="
+root = $root_data_host
+root = $root_data_vm
+ignore = Name {*.log}
+ignore = Name {.git,node_modules,.DS_Store,dump.*,stats,thumbs,tiles}
+prefer = $root_data_host
 repeat = 2
 terse = true
 dontchmod = true
@@ -35,3 +51,4 @@ fi
 cd $UNISONDIR
 [ -d .unison ] || mkdir .unison
 echo "$profile1" > .unison/lens.prf
+echo "$profile2" > .unison/lensdata.prf

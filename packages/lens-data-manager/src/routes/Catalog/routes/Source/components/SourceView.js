20dcdac031b9ce83a1edd9ed8dd37ed7cc4fef67
Dave Hamilton <djhamilton2@sbcglobal.net>
2017-03-08 23:06:29 -0600

    WIP rendering source view.

--- packages/lens-data-manager/src/routes/Catalog/routes/Source/components/SourceView.js
+++ packages/lens-data-manager/src/routes/Catalog/routes/Source/components/SourceView.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import moment from 'moment';
 
 import styles from './SourceView.scss';
 
@@ -26,31 +27,71 @@ export class SourceView extends Component {
   }
 
   renderThumbnail() {
-    const { id, stats } = this.props;
-    if (this.props.catalogLoaded) {
+    return (
+      <div className={styles.thumbnailSection}>
+        <img className={styles.thumbnail} src={this.props.thumbnailImageUrl}/>
+      </div>
+    );
+  }
+
+  renderDetails() {
+    const stats = this.props.stats || {};
+    const loading = '...';
+    const details = [
+      { l: 'Modified', v: 'ctime', c: (v) =>  moment(v).format('h:mm a, MM/DD/YY') },
+      { l: 'Size', v: 'size', c: (v) => v.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') },
+      { l: 'Format', v: 'format' },
+      { l: 'Width', v: 'width' },
+      { l: 'Height', v: 'height' },
+      { l: 'Depth', v: 'depth' },
+      { l: 'Resolution', v: 'resolution' },
+      { l: 'File Size', v: 'filesize' }
+    ];
+
+    const rows = details.map((detail, index) => {
+      let value = stats[detail.v];
+      if (value) {
+        if (detail.c) {
+          value = detail.c(value);
+        }
+      } else {
+        value = loading;
+      }
       return (
-        <div className={styles.source}>
-          test-{id}
-          <p>
-            {JSON.stringify(stats)}
-          </p>
+        <div key={index} className={styles.detailsRow}>
+          <div className={styles.detailsLabel}>{ detail.l }</div><div className={styles.detail}>{ value }</div>
         </div>
       );
-    } else {
-      return (
-        <div className={styles.source}>
-          loading test-{id}
-        </div>
-      );
-    }
+    });
+
+    return (
+      <div className={styles.detailsSection}>
+        { rows }
+      </div>
+    );
+  }
+
+  renderExplorer() {
+    return (
+      <div>
+        { 'To Do...' }
+      </div>
+    );
   }
 
   render() {
     return (
       <div className={styles.container}>
-        <img className={styles.thumbnail} src={this.props.thumbnailImageUrl}/>
-        { this.renderThumbnail() }
-        <Link to={'/catalog'}>back to catalog</Link>
+        <div className={styles.summarySection}>
+          <div className={styles.returnToCatalog}>
+            <Link to={'/catalog'}><span className='glyphicon glyphicon-circle-arrow-left'></span></Link>
+          </div>
+          { this.renderThumbnail() }
+          {this.renderDetails() }
+        </div>
+        <div className={styles.exploreSection}>
+          { this.renderExplorer() }
+        </div>
       </div>
     );
   }

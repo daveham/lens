515cc9c7a232e9b9866c57999199721ce81009a0
Dave Hamilton <djhamilton2@sbcglobal.net>
2017-07-29 16:23:41 -0500

    Adding simple tests.

---
+++ packages/lens-app/src/containers/app/view.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MemoryRouter from 'react-router/MemoryRouter';
+import View from './view';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MemoryRouter>
+      <View
+        fetchTestOne={() => {}}
+        fetchTestTwo={() => {}}
+      />
+    </MemoryRouter>,
+    div
+  );
+});

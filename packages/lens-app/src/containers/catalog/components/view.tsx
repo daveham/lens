ebb883e4afc6e02076f273f4a791075b5ec5a8fb
Dave Hamilton <djhamilton2@sbcglobal.net>
2017-10-01 22:25:16 -0500

    Converted catalog view to typescript. Added support for typescript with jest.

---
+++ packages/lens-app/src/containers/catalog/components/view.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+
+import debugLib from 'debug';
+const debug = debugLib('lens:catalog:view');
+
+interface ISourceDescriptor {
+  id: string;
+  name: string;
+  file: string;
+}
+
+interface IThumbnailDescriptor {
+  id: string;
+  file: string;
+}
+
+interface IEnsureImageLoadedPayload {
+  [name: string]: IThumbnailDescriptor;
+}
+
+interface IProps {
+  loading?: boolean;
+  loaded?: boolean;
+  name?: string;
+  sources: ReadonlyArray<ISourceDescriptor>;
+  thumbnailImageDescriptors: ReadonlyArray<IThumbnailDescriptor>;
+  requestCatalog: () => void;
+  imageLoading: (payload: IEnsureImageLoadedPayload) => void;
+}
+
+class View extends React.Component<IProps, any> {
+  public componentDidMount() {
+    const { loading, loaded } = this.props;
+    if (!(loaded || loading)) {
+      setTimeout(() => {
+        this.props.requestCatalog();
+      }, 500);
+    }
+  }
+
+  public componentDidUpdate(prevProps: IProps) {
+    const idsLoaded = (!(prevProps.thumbnailImageDescriptors && prevProps.thumbnailImageDescriptors.length > 0) &&
+      (this.props.thumbnailImageDescriptors && this.props.thumbnailImageDescriptors.length > 0));
+    if (idsLoaded) {
+      this.props.thumbnailImageDescriptors.forEach((imageDescriptor) => {
+        debug('start loading', imageDescriptor);
+        this.props.imageLoading({ imageDescriptor });
+      });
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+        <h1>Catalog</h1>
+        <div>This is the data catalog.</div>
+        {this.renderLoading()}
+        {this.renderCatalog()}
+      </div>
+    );
+  }
+
+  private renderLoading() {
+    const { loading } = this.props;
+    return (
+      loading &&
+        <div>loading...</div>
+    );
+  }
+
+  private renderCatalog() {
+    const { loaded, name, thumbnailImageDescriptors } = this.props;
+    debug('renderCatalog', { thumbnailImageDescriptors });
+    return (
+      loaded &&
+      (
+        <div>
+          <div>{name}</div>
+          {this.renderSources()}
+        </div>
+      )
+    );
+  }
+
+  private renderSources() {
+    const { sources } = this.props;
+    return (
+      <div>
+        {sources.map((source) => <div key={source.id}>{source.id} - {source.name} ({source.file})</div>)}
+      </div>
+    );
+  }
+}
+
+export default View;

dd4717e23e79bc4862772e48914f5876a80c1968
Dave Hamilton <djhamilton2@sbcglobal.net>
2018-02-05 23:01:20 -0600

    Better thumbnails that keep aspect ratio of original image.

--- packages/lens-service/service/jobs/utils/gmThumbnail.js
+++ packages/lens-service/service/jobs/utils/gmThumbnail.js
@@ -6,9 +6,9 @@ const debug = debugLib('lens:job-utils-thumbnail');
 export default (sourceFile, destFile) => {
   return new Promise((resolve, reject) => {
     try {
-      gm(sourceFile).thumb(100, 100, destFile, 80, (error) => {
+      gm(sourceFile).thumbnail(null, 100).write(destFile, (error) => {
         if (error) {
-          debug('gm thumb error', { error });
+          debug('gm thumbnail error', { error });
           return reject(error);
         }
 

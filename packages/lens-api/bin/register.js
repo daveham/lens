34411dce50d0d92f2f30bfa21d06fb2027eeec4e
Dave Hamilton <djhamilton2@sbcglobal.net>
2018-02-05 18:52:48 -0600

    Updated babel presets and config.

--- packages/lens-api/bin/register.js
+++ packages/lens-api/bin/register.js
@@ -1,14 +1,3 @@
 require('dotenv').config();
-
-require('babel-register')({
-  presets: ['es2015', 'stage-0'],
-  plugins: [
-    'add-module-exports', 'transform-runtime',
-    ['module-resolver', {
-      root: ['.']
-    }]
-  ],
-  babelrc: false
-});
-
+require('babel-register');
 require('./start');

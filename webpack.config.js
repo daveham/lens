27bff1519ab6049977fa8b43762129fbf20eb108
Dave Hamilton <djhamilton2@sbcglobal.net>
2018-03-10 16:46:44 -0600

    Adding support for CSS Styles - prod env.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const isDevelopment = process.argv.indexOf('-p') === -1;
 
@@ -57,9 +58,59 @@ if (isDevelopment) {
   plugins = [];
 } else {
   publicPath = '/build/';
+  const cssFilename = 'static/css/[name].css';
+  const extractTextPluginOptions =
+    // Making sure that the publicPath goes back to build folder.
+    { publicPath: Array(cssFilename.split('/').length).join('../') };
 
   rules = [
-    { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.(scss|sass)$/,
+      loader: ExtractTextPlugin.extract(Object.assign(
+        {
+          fallback: require.resolve('style-loader'),
+          use: [
+            {
+              loader: require.resolve('css-loader'),
+              options: {
+                importLoaders: 1,
+                modules: true,
+                minimize: true,
+                sourceMap: true
+              }
+            },
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebookincubator/create-react-app/issues/2677
+                ident: 'postcss',
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9' // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009'
+                  })
+                ]
+              }
+            },
+            require.resolve('sass-loader')
+          ]
+        },
+        extractTextPluginOptions
+      ))
+      // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+    }
   ];
 
   plugins = [
@@ -67,6 +118,9 @@ if (isDevelopment) {
       output: {
         comments: false
       }
+    }),
+    new ExtractTextPlugin({
+      filename: cssFilename
     })
   ];
 }

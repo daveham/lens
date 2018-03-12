27f269e43423a864454babea5740d70f643b85ae
Dave Hamilton <djhamilton2@sbcglobal.net>
2018-03-12 17:26:36 -0500

    Image paths again.

--- src/components/Home/Intro/index.js
+++ src/components/Home/Intro/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import styles from '../styles.scss';
 
+function makeImgUrl(url) {
+  if (process.env.NODE_ENV === 'production') {
+    return url;
+  }
+  return '/' + url;
+}
+
+const img1 = makeImgUrl('build/images/DMEvenGen500.jpg');
+const img2 = makeImgUrl('build/images/EveningCloudsDavisMountains500.jpg');
+const img3 = makeImgUrl('build/images/EveSrcCut.jpg');
+const img4 = makeImgUrl('build/images/DMEvenCut.png');
+
 export default () => {
   return (
     <div>
       <p className={styles.normal}>
         Generate this image:
       </p>
-      <img src='/build/images/DMEvenGen500.jpg' alt='' className={styles.framedImage}/>
+      <img src={img1} alt='' className={styles.framedImage}/>
       <p className={styles.normal}>
         From this photo:
       </p>
-      <img src='/build/images/EveningCloudsDavisMountains500.jpg' alt=''
+      <img src={img2} alt=''
            className={styles.framedImage}/>
       <p className={styles.normal}>
         Detail:
       </p>
       <div className={styles.cutContainer}>
         <div>
-          <img src='/build/images/EveSrcCut.jpg' alt='' className={styles.framedImage}/>
+          <img src={img3} alt='' className={styles.framedImage}/>
         </div>
         <div>
-          <img src='/build/images/DMEvenCut.png' alt='' className={styles.framedImage}/>
+          <img src={img4} alt='' className={styles.framedImage}/>
         </div>
       </div>
     </div>

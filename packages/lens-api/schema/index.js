e3d951a439c832ace28341e6d44f035521c9c431
Dave Hamilton <djhamilton2@sbcglobal.net>
2018-05-18 22:50:20 -0500

    Functions for generating mock graphql data.

--- packages/lens-api/schema/index.js
+++ packages/lens-api/schema/index.js
@@ -70,91 +70,97 @@ const typeDefs = `
   }
 `;
 
-const renderings = [
-  { id: 1, simulationId: 1, executionId: 1, name: 'sim1-ex1-ren1' },
-  { id: 2, simulationId: 1, executionId: 1, name: 'sim1-ex1-ren2' },
-  { id: 3, simulationId: 1, executionId: 2, name: 'sim1-ex2-ren1' },
-  { id: 4, simulationId: 1, executionId: 2, name: 'sim1-ex2-ren2' },
-  { id: 5, simulationId: 1, executionId: 2, name: 'sim1-ex2-ren3' },
-  { id: 6, simulationId: 1, executionId: 3, name: 'sim1-ex3-ren1' },
-  { id: 7, simulationId: 2, executionId: 4, name: 'sim2-ex1-ren1' },
-  { id: 8, simulationId: 2, executionId: 4, name: 'sim2-ex1-ren2' },
-  { id: 9, simulationId: 2, executionId: 4, name: 'sim2-ex1-ren3' },
-  { id: 10, simulationId: 2, executionId: 5, name: 'sim2-ex2-ren1' },
-  { id: 11, simulationId: 3, executionId: 6, name: 'sim3-ex1-ren1' },
-  { id: 12, simulationId: 3, executionId: 6, name: 'sim3-ex1-ren2' },
-  { id: 13, simulationId: 4, executionId: 7, name: 'sim4-ex1-ren1' },
-  { id: 14, simulationId: 4, executionId: 7, name: 'sim4-ex1-ren2' },
-  { id: 15, simulationId: 4, executionId: 8, name: 'sim4-ex2-ren1' },
-  { id: 16, simulationId: 4, executionId: 9, name: 'sim4-ex3-ren1' },
-  { id: 17, simulationId: 4, executionId: 9, name: 'sim4-ex3-ren2' },
-  { id: 18, simulationId: 4, executionId: 9, name: 'sim4-ex3-ren3' }
-];
-let nextRenderingId = 18;
+const allRenderings = [];
+const allExecutions = [];
+const allSimulations = [];
 
-const executions = [
-  { id: 1, simulationId: 1, name: 'sim1-ex1', renderings: renderings.filter(r => r.executionId === 1) },
-  { id: 2, simulationId: 1, name: 'sim1-ex2', renderings: renderings.filter(r => r.executionId === 2) },
-  { id: 3, simulationId: 1, name: 'sim1-ex3', renderings: renderings.filter(r => r.executionId === 3) },
-  { id: 4, simulationId: 2, name: 'sim2-ex1', renderings: renderings.filter(r => r.executionId === 4) },
-  { id: 5, simulationId: 2, name: 'sim2-ex2', renderings: renderings.filter(r => r.executionId === 5) },
-  { id: 6, simulationId: 3, name: 'sim3-ex1', renderings: renderings.filter(r => r.executionId === 6) },
-  { id: 7, simulationId: 4, name: 'sim4-ex1', renderings: renderings.filter(r => r.executionId === 7) },
-  { id: 8, simulationId: 4, name: 'sim4-ex2', renderings: renderings.filter(r => r.executionId === 8) },
-  { id: 9, simulationId: 4, name: 'sim4-ex3', renderings: renderings.filter(r => r.executionId === 9) }
-];
-let nextExecutionId = 10;
+let renderingIdIndex = 0;
+let executionIdIndex = 0;
+let simulationIdIndex = 0;
 
-const simulations = [
-  { id: 1, name: 'sim1', executions: executions.filter(e => e.simulationId === 1) },
-  { id: 2, name: 'sim2', executions: executions.filter(e => e.simulationId === 2)  },
-  { id: 3, name: 'sim3', executions: executions.filter(e => e.simulationId === 3)  },
-  { id: 4, name: 'sim4', executions: executions.filter(e => e.simulationId === 4)  }
-];
-let nextSimulationId = 5;
+function generateMockRendering(simulationId, executionId) {
+  const rendering = {
+    id: ++renderingIdIndex,
+    simulationId,
+    executionId,
+    name: `sim${simulationId}-ex${executionId}-ren${renderingIdIndex}`
+  };
+  allRenderings.push(rendering);
+  return rendering;
+}
+
+function generateMockExecution(simulationId, renderingCount) {
+  const execution = {
+    id: ++executionIdIndex,
+    simulationId,
+    renderings: [],
+    name: `sim${simulationId}-ex${executionIdIndex}`
+  };
+
+  for (let i = 0; i < renderingCount; i++) {
+    execution.renderings.push(generateMockRendering(simulationId, executionIdIndex));
+  }
+  allExecutions.push(execution);
+  return execution;
+}
+
+function generateMockSimulation(renderingCounts) {
+  const simulation = {
+    id: ++simulationIdIndex,
+    executions: renderingCounts.map(n => generateMockExecution(simulationIdIndex, n)),
+    name: `sim${simulationIdIndex}`
+  };
+  allSimulations.push(simulation);
+  return simulation;
+}
+
+generateMockSimulation([2, 3, 1]);
+generateMockSimulation([3, 1]);
+generateMockSimulation([2]);
+generateMockSimulation([2, 1, 3]);
 
 const resolvers = {
   Query: {
-    simulations: () => simulations,
-    simulation: (_, { id }) => simulations.find(s => s.id === id),
-    executions: () => executions,
-    execution: (_, { id }) => executions.find(e => e.id === id),
-    renderings: () => renderings,
-    rendering: (_, { id }) => renderings.find(e => e.id === id)
+    simulations: () => allSimulations,
+    simulation: (_, { id }) => allSimulations.find(s => s.id === id),
+    executions: () => allExecutions,
+    execution: (_, { id }) => allExecutions.find(e => e.id === id),
+    renderings: () => allRenderings,
+    rendering: (_, { id }) => allRenderings.find(e => e.id === id)
   },
   Mutation: {
     createSimulation: (_, { input }) => {
-      const simulation = { ...input, executions: [], id: nextSimulationId++ };
-      simulations.push(simulation);
+      const simulation = { ...input, executions: [], id: ++simulationIdIndex };
+      allSimulations.push(simulation);
       return simulation;
     },
     updateSimulation: (_, { input }) => {
-      const simulation = simulations.find((item) => item.id === input.id);
+      const simulation = allSimulations.find((item) => item.id === input.id);
       simulation.name = input.name;
       return simulation;
     },
     createExecution: (_, { input }) => {
-      const execution = { ...input, renderings: [], id: nextExecutionId++ };
-      executions.push(execution);
-      const simulation = simulations.find((item) => item.id === input.simulationId);
+      const execution = { ...input, renderings: [], id: ++executionIdIndex };
+      allExecutions.push(execution);
+      const simulation = allSimulations.find((item) => item.id === input.simulationId);
       simulation.executions.push(simulation);
       return execution;
     },
     updateExecution: (_, { input }) => {
-      const execution = executions.find((item) => item.id === input.id);
+      const execution = allExecutions.find((item) => item.id === input.id);
       execution.name = input.name;
       return execution;
       },
     createRendering: (_, { input }) => {
-      const rendering = { ...input, id: nextRenderingId++ };
-      renderings.push(rendering);
-      const execution = executions.find((item) => item.id === input.executionId &&
+      const rendering = { ...input, id: ++renderingIdIndex };
+      allRenderings.push(rendering);
+      const execution = allExecutions.find((item) => item.id === input.executionId &&
         item.simulationId === input.simulationId);
       execution.renderings.push(execution);
       return rendering;
     },
     updateRendering: (_, { input }) => {
-      const rendering = renderings.find((item) => item.id === input.id);
+      const rendering = allRenderings.find((item) => item.id === input.id);
       rendering.name = input.name;
       return rendering;
     }

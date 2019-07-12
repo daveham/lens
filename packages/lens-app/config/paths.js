5a454f33693fb11666e5c205452820a8fd037ce3
Dave Hamilton <djhamilton2@sbcglobal.net>
2019-07-11 23:41:12 -0500

    Cleaning out dead stuff after upgrade.

--- packages/lens-app/config/paths.js
+++ packages/lens-app/config/paths.js
@@ -5,7 +5,6 @@ const url = require('url');
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
-console.log('>>> appDirectory:', appDirectory);
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 const envPublicUrl = process.env.PUBLIC_URL;

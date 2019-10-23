ae655ed58b9e49d01f166c460fedd40f04845492
Dave Hamilton <dave@call-em-all.com>
2019-10-22 23:12:23 -0500

    Removing old/unused simulation view files.

--- packages/lens-app/src/containers/editor/simulation/simulationList/index.js
+++
@@ -1,4 +0,0 @@
-import SimulationTable from './simulationTable';
-import gqlWrapper from './gqlWrapper';
-
-export default gqlWrapper(SimulationTable);
--- packages/lens-app/src/containers/editor/simulation/simulationNew/index.js
+++ packages/lens-app/src/containers/editor/simulation/simulationNew/index.js
@@ -15,23 +15,3 @@ export default (props) => {
     sourceId={sourceId}
   />;
 };
-
-// import React from 'react';
-// import { backupUrl } from 'src/helpers';
-// import View from './view';
-//
-// export default (props) => {
-//   const {
-//     history,
-//     match: { url, params: { sourceId } }
-//   } = props;
-//
-//   const returnToList = () => {
-//     history.replace(backupUrl(url));
-//   };
-//
-//   return <View
-//     onClose={returnToList}
-//     sourceId={sourceId}
-//   />;
-// };

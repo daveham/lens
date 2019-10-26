3d1505644816e510d61c8ea3888ecb16c4b7e8cd
Dave Hamilton <dave@call-em-all.com>
2019-10-25 23:58:13 -0500

    Showing details of pending delete.

--- packages/lens-app/src/containers/editor/simulation/simulationDelete/view.tsx
+++ packages/lens-app/src/containers/editor/simulation/simulationDelete/view.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useSelector as useSelectorGeneric, TypedUseSelectorHook } from 'react-redux';
 
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { RootEditorState } from 'editor/modules';
 import { simulationDeleteListSelector } from 'editor/modules/selectors';
 
@@ -8,6 +15,16 @@ import Layout from '../common/layout';
 
 // import _debug from 'debug';
 // const debug = _debug('lens:editor:simulation:simulationDelete:view');
+
+const useStyles: any = makeStyles((theme: any) => ({
+  root: {
+    margin: theme.spacing(2),
+  },
+  listWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+}));
 
 interface IProps {
   simulationId?: string;
@@ -20,11 +37,27 @@ const View = ({ simulationId }: IProps) => {
     // @ts-ignore
     useSelector(state => simulationDeleteListSelector(state, simulationId));
 
+  const classes = useStyles();
+
   return (
     <Layout title='Simulation'>
-      {simulationDeleteList.map(({ key, label, name }) => (
-        <div key={key}>{`${label}: ${name}`}</div>
-      ))}
+      <Paper className={classes.root}>
+        <div className={classes.listWrapper}>
+          <List
+            subheader={
+              <ListSubheader disableSticky color='primary'>
+                These items will be deleted:
+              </ListSubheader>
+            }
+          >
+            {simulationDeleteList.map(({ key, label, name }) => (
+              <ListItem key={key}>
+                <ListItemText primary={name} secondary={`${label} ${key}`} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Paper>
     </Layout>
   );
 };

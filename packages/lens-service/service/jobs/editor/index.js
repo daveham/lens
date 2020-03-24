5aca65b07315050ab1d169be21c8180f66f8592a
Dave Hamilton <dave@call-em-all.com>
2020-03-24 00:14:01 -0500

    Starting runExecution command.

--- packages/lens-service/service/jobs/editor/index.js
+++ packages/lens-service/service/jobs/editor/index.js
@@ -1,37 +1,79 @@
 import co from 'co';
+import paths from '../../../config/paths';
 import { respond, respondWithError } from '../../../server/context';
 
 import _debug from 'debug';
+// import fileStat from '../utils/fileStat';
+import identify from '../utils/gmIdentify';
+import loadCatalog from '../utils/loadCatalog';
 const debug = _debug('lens:editor');
 
-const delayJobStep = (delay) => {
-  return new Promise((resolve) => setTimeout(() => resolve(), delay));
+const delayJobStep = delay => {
+  return new Promise(resolve => setTimeout(() => resolve(), delay));
 };
 
-function* runExecutionGenerator(job) {
-  const { payload, ...parameters } = job;
-  debug('perform runExecution', { parameters });
-  yield delayJobStep(1000);
-  respond(job, { message: 'step one' });
-  yield delayJobStep(2000);
-  respond(job, { message: 'step two' });
-  yield delayJobStep(3000);
-  respond(job, { message: 'step three' });
+function* getExecutionContext(sourceId) {
+  const catalog = yield loadCatalog();
+  const { file } = catalog[sourceId];
+  const sourceFile = paths.resolveSourcePath(file);
+  // const fileStats = yield fileStat(sourceFile);
+  const identifyResults = yield identify(sourceFile);
+  const { width, height } = identifyResults;
+  return {
+    sourceId,
+    sourceFile,
+    height,
+    width,
+  };
+}
 
-  yield Promise.resolve(true);
+function* runExecutionGenerator(job) {
+  respond(job, { message: 'execution started' });
+  const { payload, ...parameters } = job;
+  debug('perform runExecution', { payload, parameters });
+  const {
+    simulation: { sourceId },
+  } = payload;
+
+  const executionContext = yield* getExecutionContext(sourceId);
+  debug('runExecution', { executionContext });
+
+  respond(job, { message: 'hike initialized' });
+
+  yield delayJobStep(200);
+  respond(job, { message: 'trail initialized' });
+
+  yield delayJobStep(200);
+  respond(job, { message: 'hiker initialized' });
+
+  yield delayJobStep(1000);
+  respond(job, { message: 'quota 25%' });
+
+  yield delayJobStep(1000);
+  respond(job, { message: 'quota 50%' });
+
+  yield delayJobStep(1000);
+  respond(job, { message: 'quota 75%' });
+
+  yield delayJobStep(1000);
+  respond(job, { message: 'quota 100%' });
+
+  yield delayJobStep(500);
+  respond(job, { message: 'execution finished' });
+
+  // yield Promise.resolve(true);
 }
 
 const editor = jobs => {
   jobs.runExecution = {
     perform: async job => {
-      return co(runExecutionGenerator(job))
-        .catch(error => {
-          respondWithError(job, error);
-        });
+      return co(runExecutionGenerator(job)).catch(error => {
+        respondWithError(job, error);
+      });
     },
   };
 };
 
-export default (jobs) => {
+export default jobs => {
   editor(jobs);
 };

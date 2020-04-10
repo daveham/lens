f9db1ba0d4debcbd9a45de227119bb23a8901e01
Dave Hamilton <dave@call-em-all.com>
2020-04-09 21:26:21 -0500

    Structure for reporting progress during simulation execution.

--- packages/lens-service/service/jobs/editor/index.js
+++ packages/lens-service/service/jobs/editor/index.js
@@ -24,7 +24,6 @@ const delayJobStep = delay => {
 };
 
 function* initializeExecution(job) {
-  respond(job, { message: 'execution initialization started' });
   const { payload, ...parameters } = job;
   debug('perform runExecution', { payload, parameters });
   const {
@@ -44,7 +43,15 @@ function* initializeExecution(job) {
     width,
   });
 
-  yield enqueueJob(startExecutionJob(job.clientId, { simulation })).catch(error => {
+  const progress = {
+    running: false,
+    passCount: 0,
+  };
+
+  debug('initializeExecution', JSON.stringify(simulation));
+  respond(job, { progress });
+
+  yield enqueueJob(startExecutionJob(job.clientId, { simulation, progress })).catch(error => {
     respondWithError(job, error);
   });
 }
@@ -52,31 +59,69 @@ function* initializeExecution(job) {
 function* startExecution(job) {
   debug('* startExecution', { job });
   yield delayJobStep(500);
-  respond(job, { message: 'execution started' });
 
-  yield enqueueJob(runExecutionPassJob(job.clientId, {})).catch(error => {
+  const {
+    payload: { simulation, progress: priorProgress },
+  } = job;
+
+  const progress = {
+    ...priorProgress,
+    running: true,
+  };
+
+  debug('startExecution', JSON.stringify(simulation));
+  respond(job, { progress });
+
+  yield enqueueJob(runExecutionPassJob(job.clientId, { simulation, progress })).catch(error => {
     respondWithError(job, error);
   });
 }
 
 function* runExecutionPass(job) {
   debug('* runExecutionPass', { job });
-  yield delayJobStep(500);
-  respond(job, { message: 'pass complete' });
+  yield delayJobStep(100);
 
-  yield enqueueJob(finishExecutionJob(job.clientId, {})).catch(error => {
+  const {
+    payload: { simulation, progress: priorProgress },
+  } = job;
+
+  const limit = 25;
+  const running = priorProgress.passCount < limit;
+  const passCount = priorProgress.passCount + (running ? 1 : 0);
+
+  const progress = {
+    ...priorProgress,
+    running,
+    passCount,
+  };
+
+  debug('runExecutionPass', JSON.stringify(simulation));
+  respond(job, { progress });
+
+  const nextJob = running
+    ? runExecutionPassJob(job.clientId, { simulation, progress })
+    : finishExecutionJob(job.clientId, { simulation, progress });
+
+  yield enqueueJob(nextJob).catch(error => {
     respondWithError(job, error);
   });
 }
 
 function* finishExecution(job) {
   yield delayJobStep(500);
-  respond(job, { message: 'execution finished' });
+
+  const {
+    payload: { simulation, progress },
+  } = job;
+
+  debug('finishExecution', JSON.stringify(simulation));
+  respond(job, { progress });
 }
 
 const editor = jobs => {
   jobs.runExecution = {
     perform: async job =>
+      // after initialization, it will enqueue a startExecution job
       co(initializeExecution(job)).catch(error => {
         respondWithError(job, error);
       }),
@@ -84,6 +129,7 @@ const editor = jobs => {
 
   jobs.startExecution = {
     perform: async job =>
+      // after execution, it will enqueue a runExecutionPass job
       co(startExecution(job)).catch(error => {
         respondWithError(job, error);
       }),
@@ -91,6 +137,8 @@ const editor = jobs => {
 
   jobs.runExecutionPass = {
     perform: async job =>
+      // after each pass, if not done, it will enqueue another runExecutionPass job
+      // if done, it will enqueue a finishExecution jbo
       co(runExecutionPass(job)).catch(error => {
         respondWithError(job, error);
       }),
@@ -98,6 +146,7 @@ const editor = jobs => {
 
   jobs.finishExecution = {
     perform: async job =>
+      // after finishing, no other job is enqueued
       co(finishExecution(job)).catch(error => {
         respondWithError(job, error);
       }),
